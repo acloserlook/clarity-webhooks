a891ffc09985a02ad1e9be302afcf8b9a042586d
EricDAllen <github@tinnaeus.com>
2020-04-22 21:32:15 -0400

    Push logging to Azure app insights

--- PollyWebhooks/index.js
+++ PollyWebhooks/index.js
@@ -1,3 +1,7 @@
+const appInsights = require("applicationinsights");
+appInsights.setup();
+const client = appInsights.defaultClient;
+
 module.exports = async function (context, req) {
   context.log(JSON.stringify({context}));
   context.log(JSON.stringify({req}));
